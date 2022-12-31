d1b3b66e11afce5ff0e849da96c2a097df859f04
Carolina Venegas <carolinafran.vs@gmail.com>
2022-12-31 20:03:14 -0300

    fix: arreglando la base de datos

--- index.js
+++ index.js
@@ -12,33 +12,34 @@ app.use(express.static("public"));
 app.listen(3000, console.log("¡Servidor encendido!"));
 
 app.get("/", (req, res) => {
-   res.sendFile(__dirname + "/public/index.html");
-  });
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 app.get("/posts", async (req, res) => {
-    try {
-      const post = await getPost();
-      let modificado = post.map((p) => ({
-        id: p.id,
-        titulo: p.titulo,
-        url: p.img,
-        descripcion: p.description,
-      }));
-      res.json(modificado);
-    } catch (error) {
-      res.json({ message: " el post no esta disponible " });
-    }
-  });
+  try {
+    const posts = await getPost();
+    const newPosts = posts.map((p) => ({
+      id: p.id,
+      titulo: p.titulo,
+      img: p.url,
+      descripcion: p.description,
+    }));
+    res.json(newPosts);
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: " el post no esta disponible " });
+  }
+});
 
-  app.post("/posts", async (req, res) => {
-    try {
-      const { titulo, url, descripcion, likes } = req.body;
-      console.log(req.body);
+app.post("/posts", async (req, res) => {
+  try {
+    const { titulo, url, descripcion, likes } = req.body;
+    console.log(req.body);
 
-      await addPost(titulo, url, descripcion, likes);
+    await addPost(titulo, url, descripcion, likes);
 
-      res.send("Post agregado exitosamente");
-    } catch (error) {
-      res.json({ message: "no esta disponible " });
-    }
-  });
+    res.send("Post agregado exitosamente");
+  } catch (error) {
+    res.status(500).json({ message: "no esta disponible " });
+  }
+});
